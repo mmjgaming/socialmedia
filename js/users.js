f07d5cbba7c6c6850d86764d58ed0c8972013d6b
mmjgaming <muntasirjunayed64@gmail.com>
2026-03-25 19:47:30 +0600

    Update users.js

--- js/users.js
+++ js/users.js
@@ -1,1 +1,22 @@
+async function loadUsers() {
+  const { data } = await supabase.from("users").select("*");
 
+  const currentUser = await getCurrentUser();
+  const userList = document.getElementById("userList");
+  userList.innerHTML = "";
+
+  data.forEach(user => {
+    if (user.id === currentUser.id) return;
+
+    const div = document.createElement("div");
+    div.innerText = user.name;
+
+    div.onclick = () => {
+      const chatId = createChatId(currentUser.id, user.id);
+      loadMessages(chatId);
+      listenMessages(chatId);
+    };
+
+    userList.appendChild(div);
+  });
+}
